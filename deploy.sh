6253e999cafb0f33430badba022ea620b84bbf6e
stdint <travis@travis-ci.org>
2018-09-09 16:15:19 -0700

    Modify deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
+git submodule init
+git submodule update
 hugo
 git checkout master
-cp -R public .
+cp -R public/* .
 git add .
 git commit -m "deploy"
 git push --force origin master
