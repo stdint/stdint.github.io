559074029a2336238c33a531fc6d81b88e89dcdb
stdint <travis@travis-ci.org>
2018-10-19 00:12:58 -0700

    update develop

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 git submodule init
 git submodule update
+git add .
+git commit -m "update develop"
 hugo
 git checkout master
 cp -R public/* .
@@ -8,4 +10,5 @@ git add .
 git commit -m "deploy"
 git push --force origin master
 git checkout develop
+git reset --hard head
 rm -rf public
